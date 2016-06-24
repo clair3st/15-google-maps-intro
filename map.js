ae1be42e36eb5a9e5a3e2a358198a77f08619065
CLAIRE GATENBY <clairejgatenby@gmail.com>
2016-06-24 11:50:26 -0700

    map handler function added

--- map.js
+++ map.js
@@ -2,28 +2,52 @@
 
   var stylesArray = [
     {
-      featureType: "all",
+      featureType: 'all',
       stylers: [
-        { hue: "#00ffe6" },
+        { hue: '#00ffe6' },
         { saturation: -20 }
       ]
     },
     {
-      featureType: "road",
-      elementType: "geometry",
+      featureType: 'road',
+      elementType: 'geometry',
       stylers: [
         { lightness: 100 },
-        { visibility: "simplified" }
+        { visibility: 'simplified' }
       ]
     },
     {
-      featureType: "road",
-      elementType: "labels",
+      featureType: 'road',
+      elementType: 'labels',
       stylers: [
-        { visibility: "off" }
+        { visibility: 'off' }
       ]
     }
   ];
 
-module.map = map;
+  var mapOptions = {
+    zoom: 15,
+    styles: stylesArray,
+    center: new google.maps.LatLng(47.618217, -122.351832),
+    mapTypeId: google.maps.MapTypeId.STREET,
+    zoomControl: true,
+    zoomControlOptions:{
+      position: google.maps.ControlPosition.RIGHT_CENTER
+    }
+  };
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  google.maps.event.addDomListener(window, 'resize', function() {
+    var center = map.getCenter();
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(center);
+  });
+
+  var market = new google.maps.Marker({
+    position: {lat: 47.618217, lng: -122.351832},
+    map: map
+  });
+
+  module.map = map;
 })(window);
